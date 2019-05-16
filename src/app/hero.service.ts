0732ccbdba2c22d1df12ccc59201eda700c9462b
stepanbondarenko <stepan.bondarenkoo@gmail.com>
2019-05-16 19:58:52 +0300

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) {
+  }
+
+  public getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
